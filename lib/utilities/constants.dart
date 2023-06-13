0add5f50c80150049e9194a403f880379ad620d7
sayedanbia1 <sayedanbia5555@gamil.com>
2023-06-13 18:31:21 +0430

    complete constants file

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -14,6 +14,22 @@ const kButtonTextStyle = TextStyle(
   fontSize: 30.0,
   fontFamily: 'Spartan MB',
 );
+const kTextfield = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: "Enter City Name",
+  hintStyle: TextStyle(color: Colors.grey),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10),
+    ),
+    borderSide: BorderSide.none,
+  ),
+);
 
 const kConditionTextStyle = TextStyle(
   fontSize: 100.0,
