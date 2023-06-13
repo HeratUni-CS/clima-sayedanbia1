9810b77bdec8a061c10a96f8f14e6982fd5a04d9
sayedanbia1 <sayedanbia5555@gamil.com>
2023-06-13 18:27:55 +0430

    complete weather file

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,32 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+
+const apiKey = '4c5b86cf1ab33e8139ecd16a4bd5b30e';
+const openWeatherUrl='https://api.openweathermap.org/data/2.5/weather';
 class WeatherModel {
+
+  Future<dynamic> getCityWeather(String cityName) async{
+    var url=Uri.parse("$openWeatherUrl&appid=$apiKey&units=metric");
+    NetworkHelper networkHelper=NetworkHelper(url);
+    var weatherData=await networkHelper.getData();
+    return weatherData;
+  }
+
+
+  Future<dynamic> getLocationWeather()async{
+
+    location locations = location();
+    await locations.getCurrentLocation();
+    var uri = Uri.parse(
+        "$openWeatherUrl?lat=${locations.latitude}&lon=${locations.longitude}&appid=$apiKey&units=metric");
+    NetworkHelper networkHelper = NetworkHelper(uri);
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
